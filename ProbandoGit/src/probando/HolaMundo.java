0252a49b19925cb0fcf71433cd2885fe0152029c
lucia1DAW <lalu_al@yahoo.es>
2017-01-03 16:49:32 +0100

    primer cambio

--- ProbandoGit/src/probando/HolaMundo.java
+++ ProbandoGit/src/probando/HolaMundo.java
@@ -4,7 +4,7 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola Git");
+		System.out.println("Hola otra vez git");
 	}
 
 }
