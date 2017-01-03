995a2bcc8479be53b9d23b0198932ee37692ab5d
lucia1DAW <lalu_al@yahoo.es>
2017-01-03 17:38:13 +0100

    modificado main

--- ProbandoGit/src/probando/HolaMundo.java
+++ ProbandoGit/src/probando/HolaMundo.java
@@ -5,6 +5,7 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola otra vez git");
+		ClaseDePrueba c= new ClaseDePrueba("hola hola");
 	}
 
 }
