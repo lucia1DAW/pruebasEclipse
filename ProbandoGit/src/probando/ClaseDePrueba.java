6212209ef3333826d4d8221e052d103d1d111a25
lucia1DAW <lalu_al@yahoo.es>
2017-01-03 16:57:34 +0100

    con la clase de prueba

---
+++ ProbandoGit/src/probando/ClaseDePrueba.java
@@ -0,0 +1,14 @@
+package probando;
+
+public class ClaseDePrueba {
+	private String mensaje;
+	
+	public ClaseDePrueba(String mensaje) {
+		super();
+		this.mensaje = mensaje;
+	}
+
+	public String toString(){
+		return mensaje;
+	}
+}
